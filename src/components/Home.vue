6367683a2aa3659c49c42157f4988ce3ea7d468c
Bhanu Pratap Singh <bhanur05@gmail.com>
2023-06-24 19:09:39 +0530

    Setup

---
+++ src/components/Home.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    Under development
+  </div>
+</template>
+
+<script>
+import { Vue } from "vue-property-decorator";
+
+export default class Home extends Vue {
+  
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="scss" scoped>
+
+</style>
